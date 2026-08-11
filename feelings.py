77a3ff16cac7b010573369041d9b4e7cb22dd6a0
Andrey <xydakov.andrey@gmail.com>
2026-08-11 16:35:17 +0700

    change script feelings

--- feelings.py
+++ feelings.py
@@ -2,18 +2,18 @@ import sqlite3
 import time
 import os
 
-def get_load(file):
+def get_load():
     with open(f'/proc/loadavg', 'r') as f:
         data = f.read().split()
         print(data)
         return f.read()
 
-def get_mem(file):
+def get_mem():
     with open(f'/proc/meminfo', 'r') as f:
         return f.read()
 
-def get_cpu(file):
+def get_cpu():
     with open(f'/proc/stat', 'r') as f:
         return f.read()
 
-print(get_load)
+print(get_load())
